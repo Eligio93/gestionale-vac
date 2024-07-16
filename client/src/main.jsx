821d6906043ad291298bcac6171f41b4469bfc4e
Eligio93 <eligio.cristantielli@gmail.com>
2024-07-16 15:41:22 +1000

    defines router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './Home.jsx'
+import NewMachine from './NewMachine.jsx'
 import './index.css'
+
+//defines router and routes
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index:true,
+        element:<Home />
+      },
+      {
+        path:'/nuovaMacchina',
+        element:<NewMachine />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
