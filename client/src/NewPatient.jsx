c34e8a8b18a27a91b49e0aa3f680e829a6c3a932
Eligio93 <eligio.cristantielli@gmail.com>
2024-07-23 12:04:47 +1000

    creates logic to add a new patient

---
+++ client/src/NewPatient.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+
+export default function NewPatient() {
+    const [data, setData] = useState({
+        name: '',
+        lastName: '',
+        city: '',
+        phone: ''
+    })
+    const [error, setError] = useState('')
+    const [loading, setLoading] = useState(false)
+    const navigate = useNavigate();
+
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        try {
+            const result = await axios.post('http://localhost:3001/patients/newPatient', data)
+            navigate('/');
+        } catch (err) {
+            console.log(err)
+            setError(err.response.data.message)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    function handleChange(e) {
+        const { name, value } = e.target;
+        setData(prevData => ({
+            ...prevData,
+            [name]: value
+        }));
+
+    }
+    if (loading) {
+        return <p>Loading..</p>
+    }
+
+    return (
+        <form action="" className="newPatient-form form" onSubmit={handleSubmit}>
+            <h2>Aggiungi Nuovo Paziente</h2>
+            {error && <p className="error-msg">{error}</p>}
+            <label htmlFor="patient-name">Nome:</label>
+            <input type="text" minLength={2} required name="name" value={data.name} onChange={handleChange} />
+            <label htmlFor="patient-lastName">Cognome:</label>
+            <input type="text" minLength={2} required name="lastName" value={data.lastName} onChange={handleChange} />
+            <label htmlFor="patient-phone">Telefono:</label>
+            <input type="phone" required name="phone" value={data.phone} onChange={handleChange} />
+            <label htmlFor="patient-location">Citta:</label>
+            <input type="text" required name="city" value={data.city} onChange={handleChange} />
+            <button type="submit" className="green-btn">Aggiungi Paziente</button>
+        </form>
+    )
+}
