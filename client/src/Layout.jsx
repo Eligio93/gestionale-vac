01848757ea1ab3bdbc0271088ba754d6169a42b1
Eligio93 <eligio.cristantielli@gmail.com>
2024-07-24 16:33:17 +1000

    uses the Context in home page

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,13 +1,20 @@
 import Sidebar from "./components/Sidebar"
 import SearchBar from "./components/SearchBar"
 import { Outlet } from "react-router-dom"
+import { useContext } from "react"
+import { DataContext } from "./components/DataContext"
 
 export default function Layout() {
+    const {patientsList}= useContext(DataContext)
+    console.log(patientsList)
     return (
         <>
             <div className="main">
                 <Sidebar />
-                <Outlet />
+                <div className="content">
+                    <Outlet />
+                </div>
+
             </div>
         </>
 
