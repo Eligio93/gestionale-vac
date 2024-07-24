22db6e0325f6ae27c98a10129e62f68621535d7c
Eligio93 <eligio.cristantielli@gmail.com>
2024-07-24 16:32:29 +1000

    create DataContext with patients,hospitals and machines lists

---
+++ client/src/components/DataContext.jsx
@@ -0,0 +1,48 @@
+import { Children, createContext, useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+
+export const DataContext = createContext();
+
+export const DataProvider = ({ children }) => {
+    const [patientsList, setPatientsList] = useState([]);
+    const [hospitalsList, setHospitalsList] = useState([]);
+    const [machinesList, setMachinesList] = useState([]);
+
+    const apiEndpoints = [
+        { url: 'http://localhost:3001/patients', setter: setPatientsList,name:'patients' },
+        { url: 'http://localhost:3001/hospitals', setter: setHospitalsList,name:'hospitals'},
+        { url: 'http://localhost:3001/machines', setter: setMachinesList, name:'machines'},
+    ]
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await Promise.all(apiEndpoints.map((endpoint) => axios.get(endpoint.url)))
+                response.forEach((response, index) => {
+                    const target = apiEndpoints[index].name
+                    apiEndpoints[index].setter(response.data[target])
+                })
+
+            } catch (err) {
+                console.log(err)
+
+            }
+        }
+        fetchData();
+    }, [])
+
+
+    return (
+        <DataContext.Provider value={{
+            patientsList, setPatientsList,
+            hospitalsList, setHospitalsList,
+            machinesList, setMachinesList
+        }}>
+            {children}
+        </DataContext.Provider>
+    )
+
+
+
+
+}
