44618f60e70c7f89a1e760c39445cfa702f02680
Eligio93 <eligio.cristantielli@gmail.com>
2024-07-30 00:43:30 +1000

    pass reloadData through the provider

--- client/src/components/DataContext.jsx
+++ client/src/components/DataContext.jsx
@@ -14,29 +14,35 @@ export const DataProvider = ({ children }) => {
         { url: 'http://localhost:3001/hospitals', setter: setHospitalsList,name:'hospitals'},
         { url: 'http://localhost:3001/machines', setter: setMachinesList, name:'machines'},
     ]
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await Promise.all(apiEndpoints.map((endpoint) => axios.get(endpoint.url)))
-                response.forEach((response, index) => {
-                    const target = apiEndpoints[index].name
-                    apiEndpoints[index].setter(response.data[target])
-                })
 
-            } catch (err) {
-                console.log(err)
+    const fetchData = async () => {
+        try {
+            const response = await Promise.all(apiEndpoints.map((endpoint) => axios.get(endpoint.url)))
+            response.forEach((response, index) => {
+                const target = apiEndpoints[index].name
+                apiEndpoints[index].setter(response.data[target])
+            })
 
-            }
+        } catch (err) {
+            console.log(err)
+
         }
+    }
+    useEffect(() => {
         fetchData();
     }, [])
+
+    async function reloadData(){
+        await fetchData();
+    }
 
 
     return (
         <DataContext.Provider value={{
             patientsList, setPatientsList,
             hospitalsList, setHospitalsList,
-            machinesList, setMachinesList
+            machinesList, setMachinesList,
+            reloadData
         }}>
             {children}
         </DataContext.Provider>
