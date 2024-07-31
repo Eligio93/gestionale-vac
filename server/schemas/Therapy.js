858e331cce17f86fa7daaf6b6917503fdf55ba35
Eligio93 <eligio.cristantielli@gmail.com>
2024-07-31 13:41:04 +1000

    adds patient, machine and hospital as schema properties

--- server/schemas/Therapy.js
+++ server/schemas/Therapy.js
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const therapySchema = new Schema ({
-    startDate:{type:Date},
-    endDate:{type:Date},
-    referer:{
-        name:{type:String},
-        lastName:{type:String},
-        phone:{type:String}
+const therapySchema = new Schema({
+    patient: { type: Schema.Types.ObjectId, ref: 'Patient' },
+    machine: { type: Schema.Types.ObjectId, ref: 'Machine' },
+    hospital: { type: Schema.Types.ObjectId, ref: 'Hospital' },
+    startDate: { type: Date },
+    endDate: { type: Date },
+    referer: {
+        name: { type: String },
+        lastName: { type: String },
+        phone: { type: String }
     },
-    notes:{type:String},
-    archived:{type:Boolean}
+    notes: { type: String },
+    archived: { type: Boolean }
 })
 module.exports = mongoose.model('Therapy', therapySchema)
