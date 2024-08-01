891b3ad4b05bca36c9281a9119056b667bd8e221
Eligio93 <eligio.cristantielli@gmail.com>
2024-08-01 16:40:49 +1000

    adds first data to every listed element

--- client/src/components/ListEndedTherapies.jsx
+++ client/src/components/ListEndedTherapies.jsx
@@ -1,23 +1,40 @@
+import { format } from "date-fns"
+
+
 export default function ListEndedTherapies({ finishedTherapies }) {
+
+    function handleEdit(e) {
+        e.preventDefault();
+        console.log(e.target)
+    }
     return (
-        <ul>
+        <ul className="ended-therapies-list">
             {finishedTherapies.map((therapy) =>
-                <li key={therapy._id}>
+                <li key={therapy._id} className="listed-ended-therapy">
+                    <div className="info-date">
+                        <p>Terapia iniziata il <input type="date" defaultValue={format(therapy.startDate, 'yyyy-MM-dd')} disabled />{/*{format(therapy.startDate, 'dd/MM/yyyy')}*/} e terminata il {format(therapy.endDate, 'dd/MM/yyyy')}</p>
+                    </div>
+                    <div className="info-macchina">
+                        <p>Macchina: {therapy.machine.motor + ' ' + therapy.machine.serialNumber}</p>
+                    </div>
+                    <p>Assegnata a:</p>
                     {therapy.patient ? (
                         <div className="info-paziente">
-                            <p>Nome Paziente:</p>
-                            <p>{therapy.patient.name + ' ' + therapy.patient.lastName}</p>
+                            <p>{therapy.patient.name + ' ' + therapy.patient.lastName + ' di ' + therapy.patient.city + ' TEL: ' + therapy.patient.phone}</p>
                         </div>
                     ) : (
                         <div className="info-ospedale">
-                            <p>Nome Ospedale:</p>
+                            <p>Ospedale:</p>
                             <p>{therapy.hospital.name}</p>
                         </div>
 
                     )}
 
+                    <div className="btns-nav">
+                        <button>Ritira</button>
+                        <button onClick={handleEdit}>Modifica</button>
+                    </div>
 
-                    <button>Ritira</button>
                 </li>
             )}
         </ul>
