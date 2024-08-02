242b89214721a22c2dad9c123a06409c1017640b
Eligio93 <eligio.cristantielli@gmail.com>
2024-08-02 15:37:07 +1000

    uses the loading and import the ended therapy component

--- client/src/MachinesToReturn.jsx
+++ client/src/MachinesToReturn.jsx
@@ -1,19 +1,28 @@
 import { useContext } from "react"
 import { DataContext } from "./components/DataContext"
 import { isBefore } from 'date-fns'
-import ListedEndedTherapies from "./components/ListEndedTherapies"
+import EndedTherapy from "./components/EndedTherapy"
 
 
 
 export default function MachinesToReturn() {
-    const { therapiesList } = useContext(DataContext)
+    const { therapiesList, loading } = useContext(DataContext)
+    if (loading) {
+        return <p>Loading...</p>
+    }
     const today = new Date()
     const finishedTherapies = therapiesList.filter((therapy) => isBefore(therapy.endDate, today) && therapy.archived == false)
     console.log(finishedTherapies)
+
     return (
         <>
-            <ListedEndedTherapies
-                finishedTherapies={finishedTherapies} />
+            <ul className="ended-therapies-list">
+                {finishedTherapies.map((therapy) =>
+                    <EndedTherapy
+                        key={therapy._id}
+                        therapy={therapy}
+                    />)}
+            </ul>
         </>
 
     )
