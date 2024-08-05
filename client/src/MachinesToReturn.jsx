95131f1c2fdbbdc1a3917bc96e59688f0daf3fc1
Eligio93 <eligio.cristantielli@gmail.com>
2024-08-05 19:46:04 +1000

    creates success and error messages in edit therapy

--- client/src/MachinesToReturn.jsx
+++ client/src/MachinesToReturn.jsx
@@ -2,17 +2,25 @@ import { useContext } from "react"
 import { DataContext } from "./components/DataContext"
 import { isBefore } from 'date-fns'
 import EndedTherapy from "./components/EndedTherapy"
+import { useState } from "react"
 
 
 
 export default function MachinesToReturn() {
     const { therapiesList, loading } = useContext(DataContext)
+    const [successMessage, setSuccessMessage] = useState()
+    const [errorMessage,setErrorMessage]= useState()
     if (loading) {
         return <p>Loading...</p>
     }
     const today = new Date()
     const finishedTherapies = therapiesList.filter((therapy) => isBefore(therapy.endDate, today) && therapy.archived == false)
-    console.log(finishedTherapies)
+    if (successMessage) {
+        return <p className="success-msg">{successMessage}</p>
+    }
+    if(errorMessage){
+        return <p className="error-msg">{errorMessage}</p>
+    }
 
     return (
         <>
@@ -21,6 +29,9 @@ export default function MachinesToReturn() {
                     <EndedTherapy
                         key={therapy._id}
                         therapy={therapy}
+                        successMessage={successMessage}
+                        setSuccessMessage={setSuccessMessage}
+                        setErrorMessage = {setErrorMessage}
                     />)}
             </ul>
         </>
