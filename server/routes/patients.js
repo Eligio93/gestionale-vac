079b0f2b1d59584dc0eb6562a209d6f5d7d86528
Eligio93 <eligio.cristantielli@gmail.com>
2024-08-06 21:24:22 +1000

    populate with therapies in get route and format document

--- server/routes/patients.js
+++ server/routes/patients.js
@@ -3,38 +3,39 @@ const router = express.Router();
 const Patient = require('../schemas/Patient')
 
 //GET ALL PATIENTS
-router.get('/',async (req,res,next)=>{
-    const patients = await Patient.find({});
-    return res.json({patients})
+router.get('/', async (req, res, next) => {
+    const patients = await Patient.find({})
+        .populate('therapies');
+    return res.json({ patients })
 })
 
 
 
 //ADD NEW PATIENT
-router.post('/newPatient',async(req,res,next)=>{
+router.post('/newPatient', async (req, res, next) => {
     const existingPatient = await Patient.findOne({
-        name:req.body.name.toUpperCase(), 
-        lastName:req.body.lastName.toUpperCase(),
-        city:req.body.city.toUpperCase(),
+        name: req.body.name.toUpperCase(),
+        lastName: req.body.lastName.toUpperCase(),
+        city: req.body.city.toUpperCase(),
     })
-    if(existingPatient){
-        return res.status(400).json({message:"Il paziente e' gia' stato inserito"})
+    if (existingPatient) {
+        return res.status(400).json({ message: "Il paziente e' gia' stato inserito" })
     }
-    try{
+    try {
         const patient = new Patient({
-            name:req.body.name.toUpperCase(),
-            lastName:req.body.lastName.toUpperCase(),
-            phone:req.body.phone,
-            city:req.body.city.toUpperCase(),
-            inTherapy:false
+            name: req.body.name.toUpperCase(),
+            lastName: req.body.lastName.toUpperCase(),
+            phone: req.body.phone,
+            city: req.body.city.toUpperCase(),
+            inTherapy: false
         })
         await patient.save()
-        return res.json({message:'Paziente Creato Correttamente'})
+        return res.json({ message: 'Paziente Creato Correttamente' })
 
-    }catch(err){
-       return res.status(500).json({err})
+    } catch (err) {
+        return res.status(500).json({ err })
     }
-    
+
 })
 
 module.exports = router
