0174815155be24928f672863212707a62a494782
Eligio93 <eligio.cristantielli@gmail.com>
2024-08-13 22:21:04 +1000

    formats document

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,12 +1,8 @@
 import Sidebar from "./components/Sidebar"
-import SearchBar from "./components/SearchBar"
 import { Outlet } from "react-router-dom"
-import { useContext } from "react"
-import { DataContext } from "./components/DataContext"
+
 
 export default function Layout() {
-    const {patientsList}= useContext(DataContext)
-    console.log(patientsList)
     return (
         <>
             <div className="main">
@@ -14,10 +10,7 @@ export default function Layout() {
                 <div className="content">
                     <Outlet />
                 </div>
-
             </div>
         </>
-
-
     )
 }
