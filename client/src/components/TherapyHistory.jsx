98b8d45de92bc588476fb1f8f4f179a31cdb8bb1
Eligio93 <eligio.cristantielli@gmail.com>
2024-08-13 22:17:22 +1000

    init component and filled with first datas

---
+++ client/src/components/TherapyHistory.jsx
@@ -0,0 +1,34 @@
+import { useEffect } from "react"
+import { format } from "date-fns"
+
+export default function therapyHistory({ patient, hospital, machine, therapy }) {
+    return (
+        <li className="listed-therapy-history">
+            <div>
+                <p>Terapia iniziata il {format(therapy.startDate, 'dd/MM/yyyy')} e terminata il {format(therapy.endDate, 'dd/MM/yyyy')}</p>
+            </div>
+            {(patient || hospital) ? (
+                <div>
+                    <p>La macchina  assegnata aveva motore {therapy.machine.motor} con seriale {therapy.machine.serialNumber}</p>
+                    <p>Referente Terapia: {therapy.referer.name + ' '+therapy.referer.lastName} TEL: {therapy.referer.phone}</p>
+                </div>
+
+            ) : (
+                <>
+                    {therapy.patient &&
+                        <div>
+                            <p>Macchina assegnata a {therapy.patient.name + ' ' + therapy.patient.lastName + ' di ' + therapy.patient.city}</p>
+                            <p>TEL: {therapy.patient.phone}</p>
+                            <p>Note:</p>
+                            <p>{therapy.notes}</p>
+                            <p>Referente Terapia: {therapy.referer.name + ' '+therapy.referer.lastName} TEL: {therapy.referer.phone}</p>
+                        </div>}
+                    {therapy.hospital && <p>Ciao</p>}
+
+                </>
+
+            )}
+
+        </li>
+    )
+}
