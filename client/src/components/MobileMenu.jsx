3bf8c9236567f662ead0d09eb9f007495086bfb6
Eligio93 <eligio.cristantielli@gmail.com>
2024-08-19 16:57:42 +1000

    completes functionalities of the mobile menu

--- client/src/components/MobileMenu.jsx
+++ client/src/components/MobileMenu.jsx
@@ -1,20 +1,47 @@
-import { Link } from "react-router-dom"
-import { useRef } from "react"
-import { useEffect } from "react"
+import { Link, useLocation } from "react-router-dom"
+import hamburgerIcon from '../assets/hamburgerIcon.svg'
+import { useEffect, useRef, useState } from "react"
 
-export default function MobileMenu({openMenu,setOpenMenu}){
-    const menuRef = useRef()
+export default function MobileMenu() {
+    const menuRef = useRef();
+    const iconRef = useRef()
+    const location = useLocation();
+    const [openMenu, setOpenMenu] = useState(false)
+    //This useEffect closes the menu when there s a click or a tap out of the menu itself
+    useEffect(() => {
+        let handler = (e) => {
+            //if the click doesnt include the image or the menu, closes the menu
+            if (!menuRef.current.contains(e.target) && !iconRef.current.contains(e.target)) {
+                setOpenMenu(false)
+            }
+        }
+        document.addEventListener('mousedown', handler)
+        //cleaning function of the use effect
+        return () => {
+            document.removeEventListener('mousedown', handler)
+        }
+    })
+
+    useEffect(() => {
+        setOpenMenu(false)
+    }, [location.pathname])
 
     return (
-        <nav ref= {menuRef}className={openMenu ? 'mobileMenu': 'hidden'}>
-            <ul>
-                <Link to='/'><li>Home</li></Link>
-                <Link to='/nuovaMacchina'><li>Aggiungi Macchina</li></Link>
-                <Link to='/nuovoPaziente'><li>Aggiungi Paziente</li></Link>
-                <Link to='/nuovoOspedale'><li>Aggiungi Ospedale</li></Link>
-                <Link to='/nuovaTerapia'><li>Nuova Terapia</li></Link>
-                <Link to='/ritiro'><li>Macchine da ritirare</li></Link>
-            </ul>
-        </nav>
+        <div className="mobileMenu">
+            <header>
+                <img ref={iconRef} src={hamburgerIcon} alt="" onClick={() => setOpenMenu(!openMenu)} />
+            </header>
+            <nav className={openMenu ? 'mobileNav' : 'hidden'} ref={menuRef}>
+                <ul>
+                    <Link to='/'><li>Home</li></Link>
+                    <Link to='/nuovaMacchina'><li>Aggiungi Macchina</li></Link>
+                    <Link to='/nuovoPaziente'><li>Aggiungi Paziente</li></Link>
+                    <Link to='/nuovoOspedale'><li>Aggiungi Ospedale</li></Link>
+                    <Link to='/nuovaTerapia'><li>Nuova Terapia</li></Link>
+                    <Link to='/ritiro'><li>Macchine da ritirare</li></Link>
+                </ul>
+            </nav>
+        </div>
+
     )
 }
