99e00c11c08b8f2172523fbaf7ffd605ad938acb
Eligio93 <eligio.cristantielli@gmail.com>
2024-08-19 16:57:55 +1000

    import mobile menu component

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,10 +1,14 @@
 import Sidebar from "./components/Sidebar"
+import MobileMenu from "./components/MobileMenu"
+
 import { Outlet } from "react-router-dom"
+
 
 
 export default function Layout() {
     return (
         <>
+            <MobileMenu />
             <div className="main">
                 <Sidebar />
                 <div className="content">
