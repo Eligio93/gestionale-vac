6d10b2caf17a009abeff43f908583d2282c9e0df
Eligio93 <eligio.cristantielli@gmail.com>
2024-08-20 23:28:38 +1000

    sets success message and fix label/input matching

--- client/src/NewHospital.jsx
+++ client/src/NewHospital.jsx
@@ -10,6 +10,7 @@ export default function NewHospital() {
         name: '',
         city: ''
     })
+    const [success, setSuccess] = useState()
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(false)
     const navigate = useNavigate();
@@ -26,9 +27,15 @@ export default function NewHospital() {
         e.preventDefault();
         setLoading(true);
         try {
-            const result = await axios.post('http://localhost:3001/hospitals/newHospital', data)
-            reloadData();
-            navigate('/');
+            const response = await axios.post('http://localhost:3001/hospitals/newHospital', data)
+            if (response.status == 200) {
+                setSuccess(response.data.message)
+                setTimeout(() => {
+                    setSuccess()
+                    reloadData()
+                    navigate('/')
+                })
+            }
         } catch (err) {
             console.log(err)
             setError(err.response.data.message)
@@ -40,15 +47,18 @@ export default function NewHospital() {
     if (loading) {
         return <p>Loading...</p>
     }
+    if (success) {
+        return <p className="success-msg">{success}</p>
+    }
     return (
         <>
             <h2 className="title">Aggiungi Nuovo Ospedale</h2>
             <form action="" className="newHospital-form form" onSubmit={handleSubmit}>
                 {error && <p className="error-msg">{error}</p>}
-                <label htmlFor="hospital-name">Nome Ospedale:</label>
-                <input type="text" minLength={2} required name="name" value={data.name} onChange={handleChange} />
-                <label htmlFor="hospital-location">Citta:</label>
-                <input type="text" required name="city" value={data.city} onChange={handleChange} />
+                <label htmlFor="newHospital-name">Nome Ospedale:</label>
+                <input id="newHospitalName" type="text" minLength={2} name="name" value={data.name} onChange={handleChange} required />
+                <label htmlFor="newHospital-city">Citta:</label>
+                <input id="newHospital-city" type="text" name="city" value={data.city} onChange={handleChange} required />
                 <button type="submit" className="green-btn">Aggiungi Ospedale</button>
             </form>
         </>
